58bce39a236b44c1c119358f5a1f4d507598f098
Lee White <LeeWhite187@gmail.com>
2022-12-04 16:40:03 -0500

    Updated the UriService class to properly handle pre-exsting query parameters in the given route. This allows pagination to work with existing filters.
    +semver:patch

---
+++ OGA.InfraBase/OGA.InfraBase_NET6_Tests/UriService_v2_Tests.cs
@@ -0,0 +1,233 @@
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OGA.InfraBase.Services;
+using System.Collections.Generic;
+using System.Web;
+
+namespace OGA.InfraBase_NET6_Tests
+{
+    [TestClass]
+    public class UriService_Tests
+    {
+        /* Tests for the Uri Service v2 class.
+
+        // Test  1 - Construct an instance.
+         
+         
+         
+         
+         */
+
+        // Test  1 - Construct an instance.
+        [TestMethod]
+        public void Test1()
+        {
+            var u = new UriService("");
+        }
+
+        // Test  2 - Create a simple route.
+        [TestMethod]
+        public void Test2()
+        {
+            string baseurl = "http://192.168.1.203:5000";
+            string testroute = "api/test/test";
+
+            string expectedurl = baseurl + "/" + testroute;
+
+            var u = new UriService(baseurl);
+
+            var sr = u.Compose_Url_to_Route(testroute);
+
+            if (sr.ToString() != expectedurl)
+                Assert.Fail("Wrong url.");
+        }
+
+        // Test  3 - Create a route with one query parameter.
+        [TestMethod]
+        public void Test3()
+        {
+            string baseurl = "http://192.168.1.203:5000";
+            string testroute = "api/test/test";
+
+            string queryparmname = "ffff";
+            string queryparmvalue = "gggg";
+
+            string expectedurl = baseurl + "/" + testroute + "?" + queryparmname + "=" + queryparmvalue;
+
+            var u = new UriService(baseurl);
+
+            // Create the query parameter list...
+            var qpl = new List<KeyValuePair<string, string>>();
+
+            qpl.Add(new KeyValuePair<string, string>(queryparmname, queryparmvalue));
+
+            var sr = u.Compose_Url_to_Route(testroute, qpl);
+
+            if (sr.ToString() != expectedurl)
+                Assert.Fail("Wrong url.");
+        }
+
+        // Test  4 - Create a route with two query parameters.
+        [TestMethod]
+        public void Test4()
+        {
+            string baseurl = "http://192.168.1.203:5000";
+            string testroute = "api/test/test";
+
+            string queryparmname = "ffff";
+            string queryparmvalue = "gggg";
+            string queryparmname2 = "hhhh";
+            string queryparmvalue2 = "iiii";
+
+            string expectedurl = baseurl + "/" + testroute + "?" + queryparmname + "=" + queryparmvalue + "&" + queryparmname2 + "=" + queryparmvalue2;
+
+            var u = new UriService(baseurl);
+
+            // Create the query parameter list...
+            var qpl = new List<KeyValuePair<string, string>>();
+
+            qpl.Add(new KeyValuePair<string, string>(queryparmname, queryparmvalue));
+            qpl.Add(new KeyValuePair<string, string>(queryparmname2, queryparmvalue2));
+
+            var sr = u.Compose_Url_to_Route(testroute, qpl);
+
+            if (sr.ToString() != expectedurl)
+                Assert.Fail("Wrong url.");
+        }
+
+        // Test  5 - Create a paginated url with a pre-existing query parameter and a pagefilter.
+        [TestMethod]
+        public void Test5()
+        {
+            string originalUrl = "http://192.168.1.203:5000/api/test/test?hhhh=iiii";
+
+            // Normally done in startup.cs....
+            string baseurl = "http://192.168.1.203:5000";
+            var u = new UriService(baseurl);
+
+            // Get the route from the request...
+            string route = "api/test/test";
+
+            // Compose the query string...
+            string queryparmname = "ffff";
+            string queryparmvalue = "gggg";
+            string querystring_fromrequest = "?" + queryparmname + "=" + queryparmvalue;
+
+            // Compose the route and query string...
+            string route_withquery = route + querystring_fromrequest;
+
+            // Done inside the Pagination class...
+            var sr = u.GetPageUri(new SharedKernel.QueryHelpers.PaginationFilter(1, 10), route_withquery);
+
+
+            // Now, we need to confirm the query string has all the required values...
+
+            // Recover the final query string as a dictionary...
+            var qs = sr.Query;
+            var qd = HttpUtility.ParseQueryString(qs);
+
+            if (qd[queryparmname] != queryparmvalue)
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageNumber"] != "1")
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageSize"] != "10")
+                Assert.Fail("Wrong query parameter.");
+        }
+
+        // Test  6 - Create a paginated url with a pre-existing query parameters and page numbering.
+        [TestMethod]
+        public void Test6()
+        {
+            string originalUrl = "http://192.168.1.203:5000/api/test/test?hhhh=iiii";
+
+            // Normally done in startup.cs....
+            string baseurl = "http://192.168.1.203:5000";
+            var u = new UriService(baseurl);
+
+            // Get the route from the request...
+            string route = "api/test/test";
+
+            // Compose the query string...
+            string queryparmname = "ffff";
+            string queryparmvalue = "gggg";
+            string querystring_fromrequest = "?" + queryparmname + "=" + queryparmvalue;
+            // Add in some pre-existing page numbering...
+            querystring_fromrequest = querystring_fromrequest + "&" + "pageNumber" + "=" + "3";
+            querystring_fromrequest = querystring_fromrequest + "&" + "pageSize" + "=" + "2";
+
+            // Compose the route and query string...
+            string route_withquery = route + querystring_fromrequest;
+
+            // Done inside the Pagination class...
+            var sr = u.GetPageUri(new SharedKernel.QueryHelpers.PaginationFilter(1, 10), route_withquery);
+
+
+            // Now, we need to confirm the query string has all the required values...
+
+            // Recover the final query string as a dictionary...
+            var qs = sr.Query;
+            var qd = HttpUtility.ParseQueryString(qs);
+
+            if(qd.Keys.Count != 3)
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd[queryparmname] != queryparmvalue)
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageNumber"] != "1")
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageSize"] != "10")
+                Assert.Fail("Wrong query parameter.");
+        }
+
+        // Test  7 - Create a paginated url with a pre-existing query parameters and page numbering with different casing.
+        [TestMethod]
+        public void Test7()
+        {
+            string originalUrl = "http://192.168.1.203:5000/api/test/test?hhhh=iiii";
+
+            // Normally done in startup.cs....
+            string baseurl = "http://192.168.1.203:5000";
+            var u = new UriService(baseurl);
+
+            // Get the route from the request...
+            string route = "api/test/test";
+
+            // Compose the query string...
+            string queryparmname = "ffff";
+            string queryparmvalue = "gggg";
+            string querystring_fromrequest = "?" + queryparmname + "=" + queryparmvalue;
+            // Add in some pre-existing page numbering...
+            querystring_fromrequest = querystring_fromrequest + "&" + "pagenumber" + "=" + "3";
+            querystring_fromrequest = querystring_fromrequest + "&" + "pagesize" + "=" + "2";
+
+            // Compose the route and query string...
+            string route_withquery = route + querystring_fromrequest;
+
+            // Done inside the Pagination class...
+            var sr = u.GetPageUri(new SharedKernel.QueryHelpers.PaginationFilter(1, 10), route_withquery);
+
+
+            // Now, we need to confirm the query string has all the required values...
+
+            // Recover the final query string as a dictionary...
+            var qs = sr.Query;
+            var qd = HttpUtility.ParseQueryString(qs);
+
+            if(qd.Keys.Count != 3)
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd[queryparmname] != queryparmvalue)
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageNumber"] != "1")
+                Assert.Fail("Wrong query parameter.");
+
+            if (qd["pageSize"] != "10")
+                Assert.Fail("Wrong query parameter.");
+        }
+    }
+}
--- OGA.InfraBase/OGA.InfraBase_SP/Services/UriService.cs
+++ OGA.InfraBase/OGA.InfraBase_SP/Services/UriService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace OGA.InfraBase.Services
 {
@@ -43,8 +44,10 @@ namespace OGA.InfraBase.Services
         /// <returns></returns>
         public Uri Compose_Url_to_Route(string route, List<KeyValuePair<string, string>> queryparms)
         {
+            string strurl = "";
+
             // Compose the base url: host and route...
-            var _enpointUri = new Uri(string.Concat(_baseUri, route));
+            var _enpointUri = new Uri(new Uri(_baseUri), route);
 
             // Check that no query parameters were given...
             if(queryparms == null)
@@ -56,16 +59,71 @@ namespace OGA.InfraBase.Services
 
             // If here, at least one query parameter was defined.
 
-            // Iterate each query parameter, and add each one to the url...
-            string strurl = _enpointUri.ToString();
-            foreach (var qp in queryparms)
+            // Get the existing query string from the route...
+            string existingqs = _enpointUri.Query;
+
+            // See if the given route included a query string...
+            if(string.IsNullOrEmpty(existingqs))
             {
-                strurl = Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(strurl, qp.Key, qp.Value);
+                // No existing query string to work with.
+
+                // Add each query parameter to the url...
+                strurl = _enpointUri.ToString();
+                foreach (var qp in queryparms)
+                {
+                    strurl = Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(strurl, qp.Key, qp.Value);
+                }
+            }
+            else
+            {
+                // An existing query string was given as part of the route.
+                // We will make sure to not add any duplicate query parameters.
+
+                // Get a name value dictionary of the query parameters...
+                var qd = HttpUtility.ParseQueryString(existingqs);
+
+                // Copy over any passed in query parameters...
+                if(qd != null)
+                {
+                    foreach (var qp in queryparms)
+                        qd.Set(qp.Key, qp.Value);
+                }
+
+                // Now, we need to recompose the url with the updated query string...
+                var ub = new UriBuilder(_enpointUri);
+                // Update the query string...
+                ub.Query = qd.ToString();
+
+                // And, compose the final url...
+                strurl = ub.Uri.ToString();
             }
             // At this point, all parameters are pushed in, and the URL is a string.
 
             // Convert it back to a URL and return it...
             return new Uri(strurl);
+            //// Compose the base url: host and route...
+            //var _enpointUri = new Uri(string.Concat(_baseUri, route));
+
+            //// Check that no query parameters were given...
+            //if(queryparms == null)
+            //    // No query paramters were given.
+            //    return _enpointUri;
+            //if(queryparms.Count == 0)
+            //    // No query paramters were given.
+            //    return _enpointUri;
+
+            //// If here, at least one query parameter was defined.
+
+            //// Iterate each query parameter, and add each one to the url...
+            //string strurl = _enpointUri.ToString();
+            //foreach (var qp in queryparms)
+            //{
+            //    strurl = Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(strurl, qp.Key, qp.Value);
+            //}
+            //// At this point, all parameters are pushed in, and the URL is a string.
+
+            //// Convert it back to a URL and return it...
+            //return new Uri(strurl);
         }
     }
 }
